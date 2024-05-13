3181fc0857b8232b9ad122b5f6a6400932ea1d97
pedronunes1207 <1337.pedronunes@gmail.com>
2024-05-13 04:52:03 -0400

    my projects

--- app/page.js
+++ app/page.js
@@ -7,23 +7,51 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
-async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  const data = await res.json();
-
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
-  return filtered;
-};
-
 export default async function Home() {
-  const blogs = await getData();
-
+  const blogs = [
+    {
+      cover_image: "/projects/shiba.png",
+      title: "Shiba Menu",
+      url: "https://shibamemu.com/",
+      description:
+        "Where blockchain meets AI innovation! Join us in the DeFi adventure as we unleash the power of technology to revolutionize the crypto landscape. With us, the future is pawsitively bright!",
+    },
+    {
+      cover_image: "/projects/arbinu.png",
+      title: "Arbinu Staking",
+      url: "https://stake.arbinucoin.com/",
+      description:
+        "Where DeFi meets whimsy! Seamlessly bridge blockchains, explore projects, stake assets for passive income, and farm for rewards. Laugh, earn, and play in the crypto cosmos! 🐾",
+    },
+    {
+      cover_image: "/projects/altsignal.png",
+      title: "Altsignals",
+      url: "https://token.altsignals.io/",
+      description:
+        "AltSignals is set to soar with AI, ML, NLP, and sentiment analysis. 🚀 Traders benefit from automated, 24/7 trading and advanced analysis support.",
+    },
+    {
+      cover_image: "/projects/mailchain.png",
+      title: "Mailchain",
+      url: "https://https://mailchain.com//",
+      description:
+        "Mailchain makes it possible to communicate in a web3-native way. With our SDK, developers can easily add web3 email to their own projects and applications, allowing them to engage with their users in a truly web3 way.",
+    },
+    {
+      cover_image: "/projects/grow.png",
+      title: "Grow",
+      url: "https://growprotocol.net ",
+      description:
+        "Grow Protocol will provide the first real passive income in the industry. Stake USDT, Earn USDT, 25% APY, no lies. Start earning today with Grow.",
+    },
+    {
+      cover_image: "/projects/paxful.png",
+      title: "Paxful",
+      url: "https://paxful.com/",
+      description:
+        "Your gateway to the world of crypto! Buy, sell, and trade Bitcoin securely. 💼💱 Join us and embrace the future of decentralized finance.",
+    },
+  ];
   return (
     <>
       <HeroSection />
@@ -32,7 +60,7 @@ export default async function Home() {
       <Skills />
       <Projects />
       <Education />
-      {/* <Blog blogs={blogs} /> */}
+      <Blog blogs={blogs} />
     </>
-  )
-};
+  );
+}
