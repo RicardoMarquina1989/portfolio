e7a2239a5fc81ce4337bd37da06eab61e75ecaa3
RicardoMarquina1989 <ricardoandresmarquinamolina92@gmail.com>
2024-09-04 16:42:52 -0300

    update university name

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,8 +1,9 @@
 export const educations = [
   {
     id: 1,
-    title: "University of Misiones, Argentina",
+    title: "Nanyang Technological University, Singapore",
     duration: "2008  - 2013",
-    institution: "Bachelor of Engineering (BEng), Computer Software Engineering",
+    institution:
+      "Bachelor of Engineering (BEng), Computer Software Engineering",
   },
-]
+];
