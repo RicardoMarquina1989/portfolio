144e8cef4d22ca41404078e39b0bd31e3615c672
unknown <ricardoandresmarquinamolina92@gmail.com>
2024-10-14 21:20:45 +0900

    update portfolio

--- utils/data/educations.js
+++
@@ -1,9 +0,0 @@
-export const educations = [
-  {
-    id: 1,
-    title: "Nanyang Technological University, Singapore",
-    duration: "2008  - 2013",
-    institution:
-      "Bachelor of Engineering (BEng), Computer Software Engineering",
-  },
-];
